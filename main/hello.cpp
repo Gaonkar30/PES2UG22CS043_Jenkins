d9b5a779764603bea46f4816e28efe235372ac4d
AkashGGaonkar <akashggaonkar@gmail.com>
2025-03-10 16:23:28 +0530

    modified hello file

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "PES2UG22CS043 has successfully built and run this  \n";
+  cout << "PES2UG22CS043 has successfully built and run this program again  \n";
   return 0;
 }
