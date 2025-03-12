67ae12b820b4e7fcfdd5c71c026d88e12bcbe60e
AkashGGaonkar <akashggaonkar@gmail.com>
2025-03-12 09:35:57 +0530

    output cpp file

---
+++ output.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "This is my output file";
+//   cout << "Hello, Jenkins\n";
+//   cout << "PES2UG22CS043 has successfully built and run this program again  \n";
+  return 0;
+}
